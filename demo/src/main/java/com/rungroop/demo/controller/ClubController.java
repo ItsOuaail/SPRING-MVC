d490212bd218a1159ae995c530fe21f8b1a460f8
OuaailElaouad <2003waelelaouad@gmail.com>
2024-12-21 03:08:08 +0100

    adding create club

--- demo/src/main/java/com/rungroop/demo/controller/ClubController.java
+++ demo/src/main/java/com/rungroop/demo/controller/ClubController.java
@@ -1,10 +1,13 @@
 package com.rungroop.demo.controller;
 
+import com.rungroop.demo.models.Club;
 import org.springframework.ui.Model;
 import com.rungroop.demo.dto.ClubDto;
 import com.rungroop.demo.service.ClubService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -22,5 +25,18 @@ public class ClubController {
         System.out.println("Clubs: " + clubs); // Debugging
         model.addAttribute("clubs", clubs);
         return "clubs-list";
+    }
+
+    @GetMapping("/clubs/new")
+    public String createClub(Model model) {
+        Club club = new Club();
+        model.addAttribute("club", club);
+        return "clubs-create";
+    }
+
+    @PostMapping("/clubs/new")
+    public String saveClub(@ModelAttribute("club") Club club){
+        clubService.saveClub(club);
+        return "redirect:/clubs";
     }
 }
--- demo/src/main/java/com/rungroop/demo/service/ClubService.java
+++ demo/src/main/java/com/rungroop/demo/service/ClubService.java
@@ -1,9 +1,11 @@
 package com.rungroop.demo.service;
 
 import com.rungroop.demo.dto.ClubDto;
+import com.rungroop.demo.models.Club;
 
 import java.util.List;
 
 public interface ClubService {
     List<ClubDto> findAllClubs();
+    Club saveClub(Club club);
 }
